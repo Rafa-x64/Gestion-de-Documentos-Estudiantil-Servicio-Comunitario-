e356a9ecc10aab23c90e706adb5aafda997852c3
Rafa-x64 <alvarezrafaelat@gmail.com>
2026-03-20 22:38:18 -0400

    vista de dashboard al 50%

---
+++ src/SC-client/modules/dashboard/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup lang='ts'>
+import { ref } from 'vue';
+
+</script>
+<template>
+    <nav class="navbar navbar-expand-lg bg-secondary-subtle">
+        <div class="container-fluid">
+            <router-link to="" class="navbar-brand">Gestion de Documentos</router-link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <router-link to="/login" class="nav-link text-danger bg-danger-subtle">Cerrar Session</router-link>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+</template>
+<style scoped>
+
+</style>
--- src/SC-client/modules/dashboard/views/DashboardView.vue
+++ src/SC-client/modules/dashboard/views/DashboardView.vue
@@ -1,10 +1,27 @@
 <script setup lang='ts'>
 import { ref } from 'vue';
-
+import Header from '../../dashboard/components/Header.vue'
+import Footer from '../../dashboard/components/Footer.vue';
 </script>
 <template>
-    este es el dashboard
-</template>
-<style scoped>
+    <div class="container-fluid p-1 m-0">
+        <!--Header-->
+        <section class="row d-flex justify-content-center align-items-center p-1 m-0">
+            <Header/>
+        </section>
+        <!--main-->
+        <section class="row d-flex justify-content-center align-items-center p-1 m-0">
+            <div class="col-4 p-1">
 
-</style>
+            </div>
+            <div class="col-8 p-1">
+
+            </div>
+        </section>
+        <!--footer-->
+        <section class="row d-flex justify-content-center align-items-center p-1 m-0">
+            <Footer />
+        </section>
+    </div>
+</template>
+<style scoped></style>
